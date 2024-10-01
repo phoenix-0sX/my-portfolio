15593f0523246f6845fa7e50c384d569bfabc868
phoenix-0sX <lanjarl054@gmail.com>
2024-10-01 11:49:04 +0300

    my portfolio Initiation

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  compatibilityDate: '2024-07-03',
+});
